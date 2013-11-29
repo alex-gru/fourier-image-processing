6dc11c8872b161eda9825a950a820f50ac6018cc
K0M4 <Mario.Kotoy@stud.sbg.ac.at>
2013-11-29 20:03:06 +0100

    spelling fail :(

--- ImageCreator.java
+++ ImageCreator.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class ImageCreater {
+public class ImageCreator {
 	public static ArrayList<Image> createAllImages(){
 		//ToDo
 		return null;
--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Image> allImages = ImageCreater.createAllImages();
+		ArrayList<Image> allImages = ImageCreator.createAllImages();
 		Classifier c;
 		c = new Classifier(allImages,Integer.parseInt(args[0]));
 		c.classify();
